5e925b935f763d9f8f7f9b84fa55f3bc507aaef9
rijad998 <37544758+rijad998@users.noreply.github.com>
2019-07-18 18:21:35 +0200

    setup

---
+++ first-tailwind-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <p className="pt-16">Dummy text</p>
+            </div>
+        )
+    }
+}
+
+export default Header
--- first-tailwind-app/tailwind.config.js
+++ first-tailwind-app/tailwind.config.js
@@ -1,3 +1,9 @@
+let colors = {
+  'green-tab-hover' : '#3bc97b',
+  'right-green' : '#43b696',
+  'left-green' : '#24bc63'
+}
+
 module.exports = {
   theme: {
     extend: {}
