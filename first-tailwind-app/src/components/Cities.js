dfa75d5a6952700c02da768a1d94bcc9dad47b74
rijad998 <37544758+rijad998@users.noreply.github.com>
2019-07-19 04:54:24 +0200

    Update Header, Tabs

--- first-tailwind-app/src/components/Cities.js
+++ first-tailwind-app/src/components/Cities.js
@@ -4,11 +4,31 @@ import city from './images/city.jpg'
 class Cities extends Component {
     render() {
         return (
-            <div className = "pt-32 ContentWidth flex justify-center">
+            <div className = "pt-32 ContentWidth flex justify-between">
                 <div className="Card flex flex-col justify-center">
                     <div className = "CardImg">
-                        <h2 className="text-2xl">Chiang Mai</h2>
-                        <h4 className="text-lg">Thailand</h4>
+                        <h2 className="text-2xl align-middle">Chiang Mai</h2>
+                        <h4 className="text-lg items-center">Thailand</h4>
+                    </div>
+                    <div>
+                        <h3 className="font-semibold ">Info</h3>
+                        <p>Additional info</p>
+                    </div>
+                </div>
+                <div className="Card flex flex-col justify-center">
+                    <div className = "CardImg">
+                        <h2 className="text-2xl align-middle">Chiang Mai</h2>
+                        <h4 className="text-lg items-center">Thailand</h4>
+                    </div>
+                    <div>
+                        <h3 className="font-semibold ">Info</h3>
+                        <p>Additional info</p>
+                    </div>
+                </div>
+                <div className="Card flex flex-col justify-center">
+                    <div className = "CardImg">
+                        <h2 className="text-2xl align-middle">Chiang Mai</h2>
+                        <h4 className="text-lg items-center">Thailand</h4>
                     </div>
                     <div>
                         <h3 className="font-semibold ">Info</h3>
